602562e06afa35a86147e95b3dba2b35d4dd0e87
CaiKnife <caiknife@CaiKnifeMacBookPro.local>
2016-05-18 22:58:40 +0800

    redis set demo diff union inter

--- Redis/set.php
+++ Redis/set.php
@@ -27,3 +27,18 @@ Kint::dump($result);
 
 Kint::dump($rc->sismember(SET_KEY, "a"));
 Kint::dump($rc->sismember(SET_KEY, "d"));
+
+$a = [1, 2, 3, 5, 6, 7, 8, 9];
+$b = [5, 6, 7, 8, 9, 10, 11, 12];
+const SET_A = "set-a";
+const SET_B = "set-b";
+$rc->sadd(SET_A, $a);
+$rc->sadd(SET_B, $b);
+//diff - 在a里不在b里的元素
+Kint::dump($rc->sdiff(SET_A, SET_B));
+//diff - 在b里不在a里的元素
+Kint::dump($rc->sdiff(SET_B, SET_A));
+//union a和b的并集
+Kint::dump($rc->sunion(SET_A, SET_B));
+//inter a和b的交集
+Kint::dump($rc->sinter(SET_A, SET_B));
