2429cce5faf6b6feaea3cea334740b024271639d
CaiKnife <caiknife@CaiKnifeMacBookPro.local>
2016-05-19 11:23:47 +0800

    redis zset demo

---
+++ Redis/zset.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caiknife
+ * Date: 16/5/19
+ * Time: 10:27
+ */
+
+require_once "zend_autoload.php";
+require_once dirname(__DIR__) . "/vendor/autoload.php";
+
+use Predis\Client;
+
+$rc = new Client();
+$rc->select(1);
+
+const ZSET_KEY = "zset-key";
+
+$data = ["a" => 3, "b" => 10, "c" => 1];
+$rc->zadd(ZSET_KEY, $data);
+
+Kint::dump($rc->zcard(ZSET_KEY));
+
+$rc->zincrby(ZSET_KEY, 3, "c");
+
+Kint::dump($rc->zscore(ZSET_KEY, "b"));
+
+Kint::dump($rc->zcount(ZSET_KEY, 0, 5));
+
+Kint::dump($rc->zrange(ZSET_KEY, 0, -1, ["withscores" => true]));
+
+Kint::dump($rc->zrevrange(ZSET_KEY, 0, -1, ["withscores" => true]));
